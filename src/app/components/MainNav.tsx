7a1ecc9b054fcb70efea6ebd484f39af29a1d7c8
dillonzellis <dillonzellis@gmail.com>
2025-07-13 18:11:09 -0400

    add navbar

--- src/app/components/MainNav.tsx
+++ src/app/components/MainNav.tsx
@@ -1,10 +1,33 @@
+import Link from "next/link";
+
+const navLinks = [
+  {
+    title: "Dashboard Ops",
+    href: "/",
+  },
+
+  {
+    title: "COAs",
+    href: "/coas",
+  },
+  {
+    title: "Tools",
+    href: "/tools",
+  },
+];
+
 const MainNav = () => {
   return (
     <header>
       <nav>
-        <ul className="flex gap-8 border-b border-r-slate-300">
-          <li>Dashboard Ops</li>
-          <li>COAs</li>
+        <ul className="px-8 md:px-24 py-3 flex gap-8 border-b border-r-slate-300">
+          {navLinks.map((link) => (
+            <li className="">
+              <Link href={link.href} className="text-lg hover:underline">
+                {link.title}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
