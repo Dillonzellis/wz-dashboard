f1d84ad2a1fd2871c3a33ee68ba19d7ccb9d0d7b
dillonzellis <dillonzellis@gmail.com>
2025-07-13 16:36:04 -0400

    starting files

---
+++ src/app/components/MainNav.tsx
@@ -0,0 +1,14 @@
+const MainNav = () => {
+  return (
+    <header>
+      <nav>
+        <ul className="flex gap-8 border-b border-r-slate-300">
+          <li>Dashboard Ops</li>
+          <li>COAs</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNav;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Share_Tech_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const shareTechMono = Share_Tech_Mono({
+  variable: "--font-terminal",
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -25,7 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        style={{ fontFamily: "var(--font-terminal)" }}
+        className="antialiased bg-neutral-900 text-neutral-100"
       >
         {children}
       </body>
