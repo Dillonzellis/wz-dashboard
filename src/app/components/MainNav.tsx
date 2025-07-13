f3a718a93cb34d8b98378343ebefbebe32e9aaf3
dillonzellis <dillonzellis@gmail.com>
2025-07-13 18:59:06 -0400

    add key to MainNav

--- src/app/components/MainNav.tsx
+++ src/app/components/MainNav.tsx
@@ -6,8 +6,8 @@ const MainNav = () => {
     <header>
       <nav>
         <ul className="px-8 md:px-24 py-3 flex gap-8 border-b border-r-slate-300">
-          {navLinks.map((link) => (
-            <li className="">
+          {navLinks.map((link, idx) => (
+            <li className="" key={idx}>
               <Link href={link.href} className="text-lg hover:underline">
                 {link.title}
               </Link>
