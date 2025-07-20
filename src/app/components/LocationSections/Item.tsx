5beaadb62276e56fd99b4230408ac5bd26aa11f1
dillonzellis <dillonzellis@gmail.com>
2025-07-20 16:43:43 -0400

    make item cards cleaner

--- src/app/components/LocationSections/Item.tsx
+++ src/app/components/LocationSections/Item.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 
 export const Item = ({ itemLocation, imgSrc }: ItemType) => {
   return (
-    <div className="h-full border border-neutral-700 p-3">
+    <div className="h-full border border-neutral-700 px-1">
       <Image
         src={imgSrc}
         alt=""
         width={346}
         height={306}
-        className="w-full object-cover pb-4"
+        className="w-full object-cover pt-1 pb-4"
       />
-      <div>{itemLocation}</div>
+      <div className="px-3">{itemLocation}</div>
     </div>
   );
 };
