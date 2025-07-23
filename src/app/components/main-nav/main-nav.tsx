a8805515276438882c514c273c899da133d68b9d
dillonzellis <dillonzellis@gmail.com>
2025-07-22 21:55:56 -0400

    fix desktop main nav border b

--- src/app/components/main-nav/main-nav.tsx
+++ src/app/components/main-nav/main-nav.tsx
@@ -6,7 +6,6 @@ const MainNav = () => {
     <header>
       <nav className="relative">
         <div className="pointer-events-none absolute top-0 right-0 bottom-0 z-10 w-16 bg-gradient-to-l from-black/80 to-transparent" />
-
         <ul className="scrollbar-hide flex w-screen gap-8 overflow-x-scroll border-b border-r-slate-300 px-8 py-3 md:px-24">
           {navLinks.map((link, idx) => (
             <li className="whitespace-nowrap" key={idx}>
