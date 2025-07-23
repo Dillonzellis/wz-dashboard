76b6cd67a0e719b53c7fdb6a937ff8a858d12620
dillonzellis <dillonzellis@gmail.com>
2025-07-22 21:45:40 -0400

    add overflow scroll to main nav on mobile

--- src/app/components/main-nav/main-nav.tsx
+++ src/app/components/main-nav/main-nav.tsx
@@ -4,10 +4,12 @@ import { navLinks } from "./data";
 const MainNav = () => {
   return (
     <header>
-      <nav>
-        <ul className="flex gap-8 border-b border-r-slate-300 px-8 py-3 md:px-24">
+      <nav className="relative">
+        <div className="pointer-events-none absolute top-0 right-0 bottom-0 z-10 w-16 bg-gradient-to-l from-black/80 to-transparent" />
+
+        <ul className="scrollbar-hide flex w-screen gap-8 overflow-x-scroll border-b border-r-slate-300 px-8 py-3 md:px-24">
           {navLinks.map((link, idx) => (
-            <li className="" key={idx}>
+            <li className="whitespace-nowrap" key={idx}>
               <Link href={link.href} className="text-lg hover:underline">
                 {link.title}
               </Link>
