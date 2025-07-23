26fde49673e7ad212b717e0db3abe381b3528810
dillonzellis <dillonzellis@gmail.com>
2025-07-22 21:59:57 -0400

    remove main nav mask on md and up

--- src/app/components/main-nav/main-nav.tsx
+++ src/app/components/main-nav/main-nav.tsx
@@ -5,7 +5,7 @@ const MainNav = () => {
   return (
     <header>
       <nav className="relative">
-        <div className="pointer-events-none absolute top-0 right-0 bottom-0 z-10 w-16 bg-gradient-to-l from-black/80 to-transparent" />
+        <div className="pointer-events-none absolute top-0 right-0 bottom-0 z-10 w-16 bg-gradient-to-l from-black/80 to-transparent md:hidden" />
         <ul className="scrollbar-hide flex w-screen gap-8 overflow-x-scroll border-b border-r-slate-300 px-8 py-3 md:px-24">
           {navLinks.map((link, idx) => (
             <li className="whitespace-nowrap" key={idx}>
