1cde8d8a24fab979b4b026e27b2c6bf399a03c2f
Dillon Ellis <dillonzellis@gmail.com>
2025-07-26 22:28:42 -0400

    fix kb pic

--- src/components/kyle-calculator/components/kb-intro.tsx
+++ src/components/kyle-calculator/components/kb-intro.tsx
@@ -7,13 +7,13 @@ export const KbIntro = () => {
       <p className="mb-6 text-lg text-gray-400">
         Because the math is just too damn hard
       </p>
-      {/* <Image */}
-      {/*   src="/kb-calc-avatar.jpeg" */}
-      {/*   alt="Kyle Calculator Avatar" */}
-      {/*   height={200} */}
-      {/*   width={200} */}
-      {/*   className="mb-8 rounded-sm shadow-lg" */}
-      {/* /> */}
+      <Image
+        src="/kb-calc-avatar.jpg"
+        alt="Kyle Calculator Avatar"
+        height={320}
+        width={320}
+        className="mb-8 max-w-[150px] rounded-full border-1 border-neutral-100 object-contain shadow-lg"
+      />
       <div className="pb-8 text-center text-gray-300 italic">
         &quot;guys... there is uhhh an average 2.7 people per team...&quot;
       </div>
