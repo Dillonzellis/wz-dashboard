43bc05495d506bd22c279068ce3eb6cd56cde7ef
Dillon Ellis <31318165+Dillonzellis@users.noreply.github.com>
2025-08-01 14:55:40 -0400

    add bunker map zoom overlay

--- src/app/bunkers/page.tsx
+++ src/app/bunkers/page.tsx
@@ -1,19 +1,13 @@
-import BunkerCodes from "@/components/bunker-locations/bunker-codes";
+import BunkerCodes from "@/components/bunker-locations/bunker-codes/bunker-codes";
+import BunkerMap from "@/components/bunker-locations/bunker-map";
 import { MainHeading } from "@/components/ui/main-heading";
-import Image from "next/image";
 
 export default function BunkerPage() {
   return (
     <div>
       <MainHeading>Bunkers</MainHeading>
       <section className="flex flex-col items-start gap-4 lg:flex-row">
-        <Image
-          src="/bunker-locations.jpg"
-          alt=""
-          className="w-full max-w-[750px] object-contain"
-          height={1291}
-          width={1080}
-        />
+        <BunkerMap />
         <BunkerCodes />
       </section>
     </div>
