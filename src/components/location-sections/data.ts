08029a24a7a4a349a5f9579279873360f857c6c8
Dillon Ellis <dillonzellis@gmail.com>
2025-08-01 21:12:15 -0400

    fix img paths for dashboard

--- src/components/location-sections/data.ts
+++ src/components/location-sections/data.ts
@@ -6,51 +6,51 @@ export interface ItemType {
 export const basePBs: ItemType[] = [
   {
     itemLocation: "Inside café: Back counter box.",
-    imgSrc: "/base/base-pb-1.png",
+    imgSrc: "/tower-ee/base/base-pb-1.png",
   },
   {
     itemLocation: "Office upstairs: Coffee cup, left side of desk.",
-    imgSrc: "/base/base-pb-2.png",
+    imgSrc: "/tower-ee/base/base-pb-2.png",
   },
   {
     itemLocation:
       "Opposite Burger Town: Behind center couch in cardboard box, near elevator.",
-    imgSrc: "/base/base-pb-3.png",
+    imgSrc: "/tower-ee/base/base-pb-3.png",
   },
 ];
 
 export const baseKCs: ItemType[] = [
   {
     itemLocation: "Café: Kitchen, behind door on counter.",
-    imgSrc: "/base/base-kc-1.png",
+    imgSrc: "/tower-ee/base/base-kc-1.png",
   },
   {
     itemLocation:
       "Across from café: Right side of white bench near right elevator.",
-    imgSrc: "/base/base-kc-2.png",
+    imgSrc: "/tower-ee/base/base-kc-2.png",
   },
   {
     itemLocation: "Burger Town: Bar counter facing outward.",
-    imgSrc: "/base/base-kc-3.png",
+    imgSrc: "/tower-ee/base/base-kc-3.png",
   },
 ];
 
 export const middleAPBs: ItemType[] = [
   {
     itemLocation: "Center planters near zip (4 corners).",
-    imgSrc: "/middle/middle-pb-1.png",
+    imgSrc: "/tower-ee/middle/middle-pb-1.png",
   },
   {
     itemLocation:
       "Through door leading to upper stairwell: Right of red chair.",
-    imgSrc: "/middle/middle-pb-2.png",
+    imgSrc: "/tower-ee/middle/middle-pb-2.png",
   },
 ];
 
 export const middleBPBs: ItemType[] = [
   {
     itemLocation: "Top of zip in plant cluster near pyramid corner.",
-    imgSrc: "/middle/middle-pb-3.png",
+    imgSrc: "/tower-ee/middle/middle-pb-3.png",
   },
 ];
 
@@ -58,16 +58,16 @@ export const topPBs: ItemType[] = [
   {
     itemLocation:
       "Right of Pool Table Tower: Basement level, right corner near spilled cement.",
-    imgSrc: "/top/top-pb-2.png",
+    imgSrc: "/tower-ee/top/top-pb-2.png",
   },
   {
     itemLocation:
       "Left of Pool Table Tower: Rooftop level, near wood crates in corner.",
-    imgSrc: "/top/top-pb-1.png",
+    imgSrc: "/tower-ee/top/top-pb-1.png",
   },
   {
     itemLocation:
       "Opposite Pool Table Tower: Inside tower, rear shelf behind wood panel.",
-    imgSrc: "/top/top-pb-3.png",
+    imgSrc: "/tower-ee/top/top-pb-3.png",
   },
 ];
