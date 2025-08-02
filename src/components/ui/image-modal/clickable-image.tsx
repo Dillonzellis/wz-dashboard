974fb8007ae2caa2a8de2f12173f868dbfd77905
Dillon Ellis <31318165+Dillonzellis@users.noreply.github.com>
2025-08-01 21:01:15 -0400

    pass className to image-modal

--- src/components/ui/image-modal/clickable-image.tsx
+++ src/components/ui/image-modal/clickable-image.tsx
@@ -4,8 +4,12 @@ import Image from "next/image";
 import { useState } from "react";
 import ImageModal from "./image-modal";
 import type { ImageProps } from "next/image";
+import { cn } from "@/lib/utils";
 
-export default function ClickableImage({ ...imageProps }: ImageProps) {
+export default function ClickableImage({
+  className,
+  ...imageProps
+}: ImageProps) {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   return (
@@ -13,7 +17,7 @@ export default function ClickableImage({ ...imageProps }: ImageProps) {
       <Image
         {...imageProps}
         alt={imageProps.alt}
-        className="hover:cursor-pointer"
+        className={cn("hover:cursor-pointer", className)}
         onClick={() => setIsModalOpen(true)}
       />
       <ImageModal
